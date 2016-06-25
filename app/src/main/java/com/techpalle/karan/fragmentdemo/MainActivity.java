7279072d08fe6113d5695b04f40807e23cca437b
Karan <karan.kumar.c@gmail.com>
2016-06-25 17:17:05 +0530

    All the save/restore issues have been fixed.

--- app/src/main/java/com/techpalle/karan/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/techpalle/karan/fragmentdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.techpalle.karan.fragmentdemo;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -10,28 +11,43 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements MasterFragment.OnListViewItemSelectedListener {
 
     LinearLayout linearLayout;
-    String tabName;
+    String tabName;             //Store the current tab name
+    Boolean isPortrait;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initialUiSetup();
+    }
+
+    /**
+     * Create the initial UI depending on the orientation of the phone
+     */
+    private void initialUiSetup() {
         linearLayout = (LinearLayout) findViewById(R.id.container_details);
-        try{
-            if(linearLayout == null){  //Phone opened in portrait mode
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                MasterFragment masterFragment = (MasterFragment) fragmentManager.findFragmentById(R.id.container_main);
-                if(masterFragment == null){
-                    masterFragment = new MasterFragment();
+        try {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            if (linearLayout == null) {
+
+                //region Display the master fragment in portrait mode if the container is empty
+                isPortrait = true;
+                Fragment fragment = fragmentManager.findFragmentById(R.id.container_main);
+                if (!(fragment instanceof MasterFragment || fragment instanceof DetailFragment)) {
+                    MasterFragment masterFragment = new MasterFragment();
                     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                     fragmentTransaction.add(R.id.container_main, masterFragment).commit();
                 }
+                //endregion
+
             } else {
-                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                //region Display master and detail fragments if containers are empty
+                isPortrait = false;
                 MasterFragment masterFragment = (MasterFragment) fragmentManager.findFragmentById(R.id.container_master);
-                DetailFragment detailFragment= (DetailFragment) fragmentManager.findFragmentById(R.id.container_details);
-                if(masterFragment == null && detailFragment == null){
+                DetailFragment detailFragment = (DetailFragment) fragmentManager.findFragmentById(R.id.container_details);
+                if (masterFragment == null && detailFragment == null) {
                     masterFragment = new MasterFragment();
                     detailFragment = new DetailFragment();
                     FragmentTransaction fragmentTransaction1 = fragmentManager.beginTransaction();
@@ -39,42 +55,41 @@ public class MainActivity extends AppCompatActivity implements MasterFragment.On
                     fragmentTransaction1.add(R.id.container_master, masterFragment).commit();
                     fragmentTransaction2.add(R.id.container_details, detailFragment).commit();
                 }
+                //endregion
             }
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
 
         }
-
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         String tabName = savedInstanceState.getString("tabName");
-        websiteTabClicked(tabName);/*
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        if(fragmentManager.findFragmentById(R.id.container_main) instanceof MasterFragment){
-            outState.putString("tabName","");
-        } else {
-            outState.putString("tabName",tabName);
-        }*/
+        this.tabName = tabName;
+        if(!tabName.equals("")){
+            websiteTabClicked(tabName);
+        }
         super.onRestoreInstanceState(savedInstanceState);
     }
-
-    /*public void dataPassedToActivity(String data){
-        Toast.makeText(getApplicationContext(),data, Toast.LENGTH_LONG).show();
-    }*/
 
     @Override
     public void websiteTabClicked(String tabName) {
         this.tabName = tabName;
         FragmentManager fragmentManager = getSupportFragmentManager();
-        if(linearLayout == null && !tabName.equals("")){
-            DetailFragment detailFragment = new DetailFragment();
-            Bundle bundle = new Bundle();
-            bundle.putString("tabName",tabName);
-            detailFragment.setArguments(bundle);
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.container_main, detailFragment).addToBackStack(null).commit();
+        if (isPortrait) {
+            DetailFragment detailFragment;
+            if (!(fragmentManager.findFragmentById(R.id.container_main) instanceof DetailFragment)) {
+                detailFragment = new DetailFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("tabName", tabName);
+                detailFragment.setArguments(bundle);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container_main, detailFragment).addToBackStack(null).commit();
+            } else {
+                detailFragment = (DetailFragment) fragmentManager.findFragmentById(R.id.container_main);
+                detailFragment.updateTextView(tabName);
+            }
+
         } else {
             DetailFragment detailFragment = (DetailFragment) fragmentManager.findFragmentById(R.id.container_details);
             detailFragment.updateTextView(tabName);
@@ -85,10 +100,10 @@ public class MainActivity extends AppCompatActivity implements MasterFragment.On
     protected void onSaveInstanceState(Bundle outState) {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
-        if(fragmentManager.findFragmentById(R.id.container_main) instanceof MasterFragment){
-            outState.putString("tabName","");
+        if (fragmentManager.findFragmentById(R.id.container_main) instanceof MasterFragment && isPortrait) {
+            outState.putString("tabName", "");
         } else {
-            outState.putString("tabName",tabName);
+            outState.putString("tabName", tabName);
         }
         super.onSaveInstanceState(outState);
     }
